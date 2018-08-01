82604a0a6de23a69fc1aaea79e7f0c4b0d8ee102
gloriaodipo <gloriaodipo@gmail.com>
2018-08-01 19:46:34 +0300

    [FEATURE #159445803] Implement functionality for adding entry

---
+++ src/addentry.js
@@ -0,0 +1,22 @@
+import api from "./api";
+
+document.getElementById("addentryform").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const data = {
+    title: document.getElementById("title").value,
+    description: document.getElementById("description").value
+  };
+  console.log(data);
+  const token = localStorage.getItem("token");
+  api
+    .post("/user/entries", data, token)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res);
+      alert(res.message);
+      if (res.token) {
+        localStorage.setItem("success", res.message);
+      }
+    });
+});

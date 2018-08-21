2504f8f78001b62c7dad9b570502c632a7768a57
gloriaodipo <gloriaodipo@gmail.com>
2018-08-21 11:35:36 +0300

    [FEATURE] Implement get all entries

--- src/getEntries.js
+++ src/getEntries.js
@@ -4,8 +4,27 @@ const token = localStorage.getItem("token");
 api
   .get("/user/entries", token)
   .then(res => res.json())
-  .then(res => {
-    console.log(res);
-    alert(res.message);
+  .then(data => {
+    console.log(data);
+    let entries = data["entries"];
+    for (let i in entries) {
+      if (entries.hasOwnProperty(i)) {
+        let entry = entries[i];
+        console.log(entry);
+        document.getElementById("posts").innerHTML += `<div>
+        <h4><i>${entry.title}</i></h4>
+        <p>${entry.description}</p>
+        </div><hr/>`;
+      }
+    }
+    // for (i = 0; i < data.length; i++) {
+    //   console.log(data.length);
+    //   document.getElementById("posts").innerHTML += `<div>
+    //   <h4><i>${data[i].title}</i></h4>
+    //   <p>${data[i].description}</p>
+    //   </div><hr/>`;
+    // }
   })
-  .catch(err => console.error(err));
+  .catch(error => {
+    console.log(error);
+  });
